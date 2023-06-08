b6efc84488023a5ff17f1d162c9bdc0153eab9b8
topdev <shouldperfect330@gmail.com>
2023-06-08 17:37:42 +0300

    ccc

--- sbackend/_helpers/db.js
+++ sbackend/_helpers/db.js
@@ -9,7 +9,7 @@ initialize();
 async function initialize() {
     console.log("DBInitialize called!")
     // create db if it doesn't already exist
-    const { host, port, user, password, database } = config.databaseProdcution;
+    const { host, port, user, password, database } = config.database_prodcution;
     const connection = await mysql.createConnection({ host, port, user, password });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
--- sbackend/server.js
+++ sbackend/server.js
@@ -12,7 +12,12 @@ app.use(cookieParser());
 
 // allow cors requests from any origin and with credentials
 app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
-
+//
+app.use(express.static(__dirname + '/public'));
+app.get('/test', (req, res) => {
+  console.log("arrived!")
+  res.send("hello@");
+});
 // api routes
 app.use('/accounts', require('./accounts/accounts.controller'));
 app.use('/grants', require('./modules/grants.controller'));
@@ -26,5 +31,6 @@ app.use('/proposals', require('./modules/proposals.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+// const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = 3000;
 app.listen(port, () => console.log('Server listening on port ' + port));
